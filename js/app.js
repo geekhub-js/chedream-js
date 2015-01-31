9d27df7edd0c05aa428dc7ed7ba319666585b98b
kikill95 <kirill.gus@gmail.com>
2015-01-31 10:15:15 +0200

    Editing routing in app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 angular.module('Che-app', [
+    'allDreams.routing',
+    'contacts.routing',
+    'creatingDream.routing',
+    'dream.routing',
+    'editingDream.routing',
+    'error.routing',
+    'faq.routing',
     'mainPage.routing',
+    'oldBrowser.routing',
+    'profile.routing',
+    'search.routing',
     'ui.router'
 ])
     .config(function ($urlRouterProvider) {
