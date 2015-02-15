4708136d8e9e39f62ca6dc9b152faccf25d26c16
kikill95 <kirill.gus@gmail.com>
2015-02-16 00:29:47 +0200

    Editing angular-code for faq-page: finishing checker

--- js/faq/controller.js
+++ js/faq/controller.js
@@ -3,4 +3,9 @@ angular.module('faq.controller', [
 ])
     .controller('FaqCtrl', function($scope, faq) {
         $scope.questions = faq.getQuestions();
+        $scope.changeChosenNumber = function(el) {
+            for ($scope.chosenNumber = 0; $scope.questions[$scope.chosenNumber].id !== el.id; $scope.chosenNumber++) {
+
+            }
+        };
     });
--- js/faq/model.js
+++ js/faq/model.js
@@ -30,48 +30,6 @@ angular.module('faq.model', [
                 "question": "In another moment that it was too small, but at the end of your flamingo. Shall I try the patience.",
                 "answer": "So she began: 'O Mouse, do you want to see some meaning in it,' said the Caterpillar; and it said nothing. 'This here young lady,' said the Gryphon. 'We can do without lobsters, you know. Come on!'.",
                 "slug": "rerum"
-            },
-            {
-                "id": 58,
-                "title": "alias",
-                "question": "I think you'd better finish the story for yourself.' 'No, please go on!' Alice said very humbly;.",
-                "answer": "So they got settled down again, the Dodo replied very gravely. 'What else have you executed, whether you're nervous or not.' 'I'm a poor man, your Majesty,' he began, 'for bringing these in: but I.",
-                "slug": "alias"
-            },
-            {
-                "id": 59,
-                "title": "nihil",
-                "question": "THE.",
-                "answer": "The Mouse did not like to drop the jar for fear of their hearing her; and when she had gone through that day. 'That PROVES his guilt,' said the Gryphon, 'that they WOULD not remember the simple.",
-                "slug": "nihil"
-            },
-            {
-                "id": 60,
-                "title": "nemo",
-                "question": "Hatter: 'as the things being alive; for instance, there's the arch I've got to?' (Alice had been.",
-                "answer": "There was exactly one a-piece all round. (It was this last remark. 'Of course they were', said the last concert!' on which the cook had disappeared. 'Never mind!' said the King. Here one of the.",
-                "slug": "nemo"
-            },
-            {
-                "id": 61,
-                "title": "illum",
-                "question": "Seven. 'Yes, it IS his business!' said Five, in a great thistle, to keep back the wandering hair.",
-                "answer": "Majesty,' said the last word with such a capital one for catching mice--oh, I beg your pardon,' said Alice more boldly: 'you know you're growing too.' 'Yes, but some crumbs must have imitated.",
-                "slug": "illum"
-            },
-            {
-                "id": 62,
-                "title": "qui",
-                "question": "Dormouse, who was gently brushing away some dead leaves that lay far below her. 'What CAN all that.",
-                "answer": "Alice, and looking at the top of his pocket, and was going on, as she went hunting about, and make one repeat lessons!' thought Alice; 'I can't go no lower,' said the King. 'I can't remember half of.",
-                "slug": "qui"
-            },
-            {
-                "id": 63,
-                "title": "quo",
-                "question": "The miserable Hatter dropped his teacup instead of onions.' Seven flung down his cheeks, he went.",
-                "answer": "But she did not at all the things I used to queer things happening. While she was looking at Alice for protection. 'You shan't be able! I shall ever see such a wretched height to be.' 'It is wrong.",
-                "slug": "quo"
             }
         ];
         /*
