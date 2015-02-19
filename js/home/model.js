89e3448e592064ab03ca9019bc1392bfebe3f450
kikill95 <kirill.gus@gmail.com>
2015-02-19 10:38:39 +0200

    Adding basic dream-functional for main page

--- js/home/model.js
+++ js/home/model.js
@@ -1,1 +1,31 @@
-angular.module('home.model', []);
+angular.module('home.model', [])
+    .factory('dreams', function () {
+        var dreams = [
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 51,
+                rating: 4
+            }
+        ];
+
+        dreams.forEach(function(el) {
+            if (el.favorites_count > 1) {
+                el.favorites_count_word = 'Підтримало';
+            } else if (el.favorites_count == 1) {
+                el.favorites_count_word = 'Підтримав';
+            } else {
+                el.favorites_count_word = '';
+                el.favorites_count = '';
+            }
+        });
+
+
+        return {
+            getDreams: function () {
+                return dreams;
+            }
+        };
+});
