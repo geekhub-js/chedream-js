9fbe5744bac7ed124d677d33d0b362be9bcd03e7
kikill95 <kirill.gus@gmail.com>
2015-02-19 12:12:28 +0200

    Adding spinner, api for dreams on main page and testing updating dreams on scrolling down

--- js/home/model.js
+++ js/home/model.js
@@ -1,6 +1,64 @@
-angular.module('home.model', [])
-    .factory('dreams', function () {
+angular.module('home.model', [
+    'cheDream.api'
+])
+    .factory('dreams', function (api) {
         var dreams = [
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 51,
+                rating: 4
+            },
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 1,
+                rating: 4
+            },
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 0,
+                rating: 4
+            },
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 51,
+                rating: 4
+            },
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 51,
+                rating: 4
+            },
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 51,
+                rating: 4
+            },
+            {
+                title: 'Реконструкція скверу за будівлею облдержадміністрації',
+                avatar: 'images/author.png',
+                author: 'Інна Кулік',
+                picture: 'images/img.png',
+                favorites_count: 51,
+                rating: 4
+            },
             {
                 title: 'Реконструкція скверу за будівлею облдержадміністрації',
                 avatar: 'images/author.png',
@@ -10,6 +68,12 @@ angular.module('home.model', [])
                 rating: 4
             }
         ];
+
+        /*
+         var dreams = {};
+         api('/dreams.json?limit=8').then(function (data) {
+            dreams = data.items;
+         });*/
 
         dreams.forEach(function(el) {
             if (el.favorites_count > 1) {
@@ -22,6 +86,20 @@ angular.module('home.model', [])
             }
         });
 
+        //TODO test it
+        /*
+        angular.element( document.querySelector( window ) ).bind('scroll', bindScroll);
+        function bindScroll() {
+            if (window.innerHeight + document.body.scrollTop > document.body.offsetHeight - 120) {
+                var newDreams = {},
+                    apiSting = '/dreams.json?limit=' + (dreams.length + 4);
+                api(apiSting).then(function (data) {
+                    newDreams = data.items.splice(dreams.length, 4);
+                });
+                dreams.concat(newDreams);
+            }
+        }
+        */
 
         return {
             getDreams: function () {
