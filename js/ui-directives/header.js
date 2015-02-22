72532db2c2981ceff8a42d12c95b4946d143e980
kikill95 <kirill.gus@gmail.com>
2015-02-22 23:19:59 +0200

    Finishing coding of search-block

--- js/ui-directives/header.js
+++ js/ui-directives/header.js
@@ -6,7 +6,10 @@ chedreamApp.directive('uiHeader', function() {
                 angular.element( document.querySelector( '#social-title-area' )).css('display', 'none');
                 document.querySelector( '#search-block' ).className = 'searching';
                 document.querySelector( '.field-search' ).focus();
-
+            });
+            angular.element( document.querySelector( '.field-search' ) ).on('focusout', function() {
+                angular.element( document.querySelector( '#social-title-area' )).css('display', 'block');
+                document.querySelector( '#search-block' ).className = '';
             });
         },
         templateUrl: 'templates/header.html'
