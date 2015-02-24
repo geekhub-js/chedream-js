4de0bbf896395dadd0253a67e31ef357e739d8d9
Alex Moshta <ahonymous@gmail.com>
2015-02-24 21:30:58 +0200

    connected to server

--- js/faq/controller.js
+++ js/faq/controller.js
@@ -2,7 +2,7 @@ angular.module('faq.controller', [
     'faq.model'
 ])
     .controller('FaqCtrl', function($scope, faq) {
-        $scope.questions = faq.getQuestions();
+        $scope.questions = faq.getQuestions;
         $scope.changeChosenNumber = function(el) {
             for ($scope.chosenNumber = 0; $scope.questions[$scope.chosenNumber].id !== el.id; $scope.chosenNumber++) {}
 
