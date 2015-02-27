ebcf2113522deb6b8b94624751bde08bcf00e62d
Alex Moshta <ahonymous@gmail.com>
2015-02-27 22:13:33 +0200

    refactored spinner

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var concatCss = require('gulp-concat-css');
 var minifyCSS = require('gulp-minify-css');
 
 gulp.task('default', function () {
-    gulp.src(['styles/*.css', 'bower_components/angular-loading/angular-loading.css'])
+    gulp.src(['bower_components/angular-loading/angular-loading.css', 'styles/*.css'])
         .pipe(concatCss("style.css"))
         .pipe(gulp.dest('css/'))
         .pipe(minifyCSS({keepBreaks:true}));
--- js/api/api.js
+++ js/api/api.js
@@ -5,7 +5,18 @@ angular.module('cheDream.api', [])
     .factory('api', function(server, $http, $loading) {
         return function(url, name) {
             $loading.setDefaultOptions({
-                className: 'loader'
+                text: 'Loading ' + name,
+                className: 'loader',
+                spinnerOptions: {
+                    radius: 20,
+                    length: 0,
+                    lines: 30,
+                    corners: 0.5,
+                    color: 'white',
+                    className: 'dw-spinner',
+                    top: 'auto',
+                    left: 'auto'
+                }
             });
             $loading.start(name);
             return $http.get(server + url).then(function(result) {
