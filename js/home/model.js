3cf7691f42fb2aa3bc302be6fff7982b00a83c62
kikill95 <kirill.gus@gmail.com>
2015-03-28 10:45:25 +0200

    moving function for formating dream in global scope

--- js/home/model.js
+++ js/home/model.js
@@ -7,7 +7,9 @@ angular.module('home.model', [
          var dreams = [];
          api('/dreams.json?count=8', 'dreams').then(function (data) {
             dreams = data.dreams;
-            progressOutput(dreams);
+            dreams.forEach(function(dream) {
+                dream = progressOutput(dream);
+            });
          });
 
         angular.element( window ).on('scroll', bindScroll);
@@ -16,53 +18,15 @@ angular.module('home.model', [
                 api('/dreams.json?count=' + (dreams.length + 4), 'New Dreams').then(function (data) {
                     dreams = dreams.concat(data.dreams.splice(dreams.length, 4));
 
-                    progressOutput(dreams);
+                    dreams.forEach(function(dream) {
+                        dream = progressOutput(dream);
+                    });
                     if (dreams.length % 4 !== 0) {
                         isMoreDreams = false;
                         angular.element( window ).off('scroll', bindScroll);
                     }
                 });
             }
-        }
-
-        function progressOutput(dreams) {
-            dreams.forEach(function(dr) {
-                var needWords = [
-                        'dream_financial_resources',
-                        'dream_equipment_resources',
-                        'dream_work_resources'
-                    ],
-                    haveWords = [
-                        'dream_financial_contributions',
-                        'dream_equipment_contributions',
-                        'dream_work_contributions'
-                    ],
-                    putWords = [
-                        'financialPercent',
-                        'equipmentPercent',
-                        'workPercent'
-                    ],
-                    need,
-                    have,
-                    i;
-                for (i = 0; i < putWords.length; i++) {
-                    need = 0;
-                    have = 0;
-                    dr[needWords[i]].forEach(function (el) {
-                        need += el.quantity;
-                    });
-                    dr[haveWords[i]].forEach(function (el) {
-                        have += el.quantity;
-                    });
-                    dr[putWords[i]] = Math.round( have / need * 100 );
-                    if (dr[putWords[i]] > 100) {
-                        dr[putWords[i]] = 100;
-                    }
-                    if (isNaN(dr[putWords[i]])) {
-                        dr[putWords[i]] = 0;
-                    }
-                }
-            });
         }
 
         return {
@@ -74,3 +38,42 @@ angular.module('home.model', [
             }
         };
 });
+
+function progressOutput(dream) {
+    var needWords = [
+            'dream_financial_resources',
+            'dream_equipment_resources',
+            'dream_work_resources'
+        ],
+        haveWords = [
+            'dream_financial_contributions',
+            'dream_equipment_contributions',
+            'dream_work_contributions'
+        ],
+        putWords = [
+            'financialPercent',
+            'equipmentPercent',
+            'workPercent'
+        ],
+        need,
+        have,
+        i;
+    for (i = 0; i < putWords.length; i++) {
+        need = 0;
+        have = 0;
+        dream[needWords[i]].forEach(function (el) {
+            need += el.quantity;
+        });
+        dream[haveWords[i]].forEach(function (el) {
+            have += el.quantity;
+        });
+        dream[putWords[i]] = Math.round(have / need * 100);
+        if (dream[putWords[i]] > 100) {
+            dream[putWords[i]] = 100;
+        }
+        if (isNaN(dream[putWords[i]])) {
+            dream[putWords[i]] = 0;
+        }
+    }
+    return dream;
+}
