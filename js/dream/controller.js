1e79bbb6609a97c97af9ebfbdaf95cc24cd82488
kikill95 <kirill.gus@gmail.com>
2015-03-28 14:45:10 +0200

    Adding ngSanitize for parsing string into html

--- js/dream/controller.js
+++ js/dream/controller.js
@@ -1,9 +1,13 @@
 angular.module('dream.controller', [
-    'dream.model'
+    'dream.model',
+    'ngSanitize'
 ])
-    .controller('DreamCtrl', function($scope, dream, $rootScope) {
+    .controller('DreamCtrl', function($scope, dream, $sce, $rootScope) {
 
         $rootScope.title = dream.slug;
         $scope.currentDream = dream.getDream;
+        $scope.deliberatelyTrustDangerousSnippet = function() {
+            return $sce.trustAsHtml($scope.dream.description);
+        };
 
     });
